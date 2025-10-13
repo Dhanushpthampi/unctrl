8b03ca567b9a723f7de2c201ff91aba147a8414c
dhanushpthampitopgrep <dhanush.pthampi@topgrep.com>
2025-10-13 21:53:41 +0530

    Added SpeedInsites correctly

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,7 +55,7 @@ export default function RootLayout({ children, showIntroOverlay }) {
           <SiteHeader />
           {children}
           <ScrollDownIndicator />
-          <Anlytics />
+          <Analytics />
            <SpeedInsights />
         </ScrollEffects>
       </body>
